29e73415322f82be90dec1e799dff1b994aed900
CChiu28 <ScorpiusDX@gmail.com>
2018-01-30 23:21:06 -0500

    push

--- HW1/Main3.java
+++ HW1/Main3.java
@@ -1,10 +1,11 @@
 import java.math.*;
+import java.util.Random;
 
 enum Month {
 	January, February, March, April, June, July, August, September, November, December;
 }
 public class Main3 {
-	public static void Main3(String[] args) {
-		
+	public static void main(String[] args) {
+
 	}
 }
