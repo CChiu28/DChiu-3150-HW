eec8d749e04415921230a22763ea363414111f18
CChiu28 <ScorpiusDX@gmail.com>
2018-01-31 13:59:05 -0500

    main5 push

---
+++ HW1/Main5.java
@@ -0,0 +1,15 @@
+import java.math.*;
+import java.util.Scanner;
+// use string.split
+public class Main5 {
+  public static void main(String[] args) {
+    Scanner cin = new Scanner(System.in);
+    String userInput;
+    int a1, a2;
+    int b1, b2;
+    int c1, c2;
+    System.out.print("Enter coordinates for point A - ie. x,y: ");
+    userInput = cin.nextLine();
+    System.out.print(userInput);
+  }
+}
