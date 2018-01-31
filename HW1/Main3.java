3cd017062d4728207a2924a3f90efa55b5fe15f6
CChiu28 <ScorpiusDX@gmail.com>
2018-01-31 00:13:36 -0500

    Q3

--- HW1/Main3.java
+++ HW1/Main3.java
@@ -1,11 +1,12 @@
-import java.math.*;
 import java.util.Random;
 
-enum Month {
-	January, February, March, April, June, July, August, September, November, December;
-}
+// enum Month {
+// 	January, February, March, April, June, July, August, September, November, December;
+// }
 public class Main3 {
 	public static void main(String[] args) {
-
+		Random rand = new Random();
+		int month = rand.nextInt(12);
+		System.out.println(month);
 	}
 }
