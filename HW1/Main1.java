d11357c1acfad170412eb965e142b37ec9c962b0
CChiu28 <ScorpiusDX@gmail.com>
2018-02-01 18:30:52 -0500

    Final

---
+++ HW1/Main1.java
@@ -0,0 +1,6 @@
+public class Main1 {
+  public static void main(String[] args) {
+    System.out.print("I have read and agree to the policies indicated in the syllabus and the Brooklyn College academic integrity guidelines");
+    System.exit(0);
+  }
+}
