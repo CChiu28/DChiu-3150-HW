831db6aeb1bbc613467986e1ededdd9fd8823e5f
CChiu28 <ScorpiusDX@gmail.com>
2018-02-08 23:58:29 -0500

    hw2

--- HW2/Main3.java
+++ HW2/Main3.java
@@ -4,14 +4,19 @@ public class Main3 {
   public static void main(String[] args) {
     Scanner cin = new Scanner(System.in);
     String word;
-    char[] chArray;
+    boolean palinCheck = true;
     System.out.print("Enter a word: ");
     word = cin.nextLine();
+    word = word.toLowerCase();
     int len = word.length();
     for (int i=0; i<(len/2); i++) {
-      if (word.charAt[i]!=word.charAt[len-1])
+      if (word.charAt(i)!=word.charAt(len-i-1)) {
         System.out.print(word+" is not a palindrome");
-      else System.out.print(word+" is a palindrome");
+        palinCheck = false;
+        break;
+      }
     }
+    if (palinCheck)
+      System.out.print(word+" is a palindrome");
   }
 }
