05293ad0fbecb9f49ae7f9f94e100e9855c2c53e
CChiu28 <ScorpiusDX@gmail.com>
2018-02-09 22:57:55 -0500

    push

--- HW2/Main2.java
+++ HW2/Main2.java
@@ -9,7 +9,8 @@ public class Main2 {
     String day = "";
     String[] calendar = {"","January","February","March","April","May","June","July","August","September","October","November","December"};
     int[] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    String[] week = new String[] {"","sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
+    //int[][][] monthGrid = new int[13][5][8];
+    String[] week = {"","sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
     int startWeek = 0;
     System.out.print("Enter a year: ");
     do {
@@ -22,32 +23,46 @@ public class Main2 {
           cin.next();
         }
     } while (!inputCheck);
-    if ((year%4==0)&&(year%100!=0)) {
-      days[3] = 29;
-    } else if (year%400==0)
-      days[3] = 29;
+    if (((year%4==0)&&(year%100!=0))||(year%400==0))
+      days[2] = 29;
     System.out.print("Enter the day the first day of January falls on: ");
     day = cin.next();
-    while (!day.toLowerCase().matches("(monday|tuesday|wednesday|thursday|friday|saturday|sunday)")) {
+    while (!day.toLowerCase().matches("(sunday|monday|tuesday|wednesday|thursday|friday|saturday)")) {
       System.out.print("Please enter a valid day: ");
       day = cin.next();
     }
     for (int i=1; i<=week.length-1; i++) {
       //System.out.println("test "+week[i]+" "+day+" end test");
-      if (day.toLowerCase().compareTo(week[i])==0) {
-        System.out.println(week[i]);
+      if (day.compareToIgnoreCase(week[i])==0) {
+        System.out.println(week[i]+" "+i);
         startWeek = i;
         break;
       }
     }
+    // System.out.print(monthGrid.length);
+    // for (int months=1; months<monthGrid.length; months++) {
+    //   for (int weeks=0; weeks<monthGrid[0].length; weeks++) {
+    //     for (int k=1; k<=days[months]; k++) {
+    //       if (weeks==0)
+    //         monthGrid[months][weeks][k]
+    //     }
+    //   }
+    // }
     for (int i=1; i<=calendar.length-1; i++) {
       System.out.printf("%4c%s %d\n", ' ', calendar[i], year);
       System.out.println("Su Mo Tu We Th Fr Sa");
-      for (int j=startWeek; j<7; j++)
-        System.out.printf(" %s", "  ");
-      for (int k=1; k<=days[i]; k++)
+      for (int j=1; j<startWeek; j++)
+        System.out.printf("%s", "   ");
+      for (int k=1; k<=days[i]; k++) {
         System.out.printf("%2d%s", k, " ");
-        if ()
+        if ((k+startWeek-1)%7==0)
+          System.out.println("");
+        if (k==days[i]) {
+          System.out.println("");
+          startWeek = (k+startWeek)%7;
+          System.out.println("newstart "+startWeek);
+        }
+      }
     }
   }
 }
