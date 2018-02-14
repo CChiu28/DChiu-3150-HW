87b6fdbf9b488bac31cc835536cf5579764c7b47
CChiu28 <ScorpiusDX@gmail.com>
2018-02-13 19:38:16 -0500

    commit

--- HW3/MyString.java
+++ HW3/MyString.java
@@ -1,21 +1,21 @@
 public class MyString {
   char[] charArray;
+  int index=0;
   public MyString(char[] chars) {
-    for (int i=0; i<chars.length; i++) {
-      charArray[i] = chars[i];
-    }
+    this.charArray = chars;
   }
   public char charAt(int index) {
     return charArray[index];
   }
   public int length() {
-    return charArray.length;
+    return this.charArray.length;
   }
   public MyString substring(int begin, int end) {
-    MyString newSub = new MyString();
-    for (int i=charArray[begin]; i<charArray[end]; i++) {
-      newSub(charArray[i]);
+    char[] temp = new char[end-begin];
+    for (int i=begin; i<end; i++) {
+      temp[i-begin] = this.charArray[begin];
     }
+    return MyString(temp);
   }
   public MyString toLowerCase() {
   }
