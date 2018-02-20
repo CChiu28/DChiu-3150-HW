5a4dcfbcc94a4956e71936c3a732e69ba5df3851
CChiu28 <ScorpiusDX@gmail.com>
2018-02-19 20:42:10 -0500

    Hw3

--- HW3/Main.java
+++ HW3/Main.java
@@ -1,24 +1,22 @@
 public class Main {
   public static void main(String[] args) {
-    char[] array = {'T','E','S','T','l','o','w','e','r'};
-    MyString test = new MyString(array);
-    char[] array2 = {'h','e','l','l','o'};
-    MyString test2 = new MyString(array2);
-    MyString test3 = new MyString(array);
-    MyString test4 = test3.valueOf('4');
-    String test5 = test2.toString();
-    System.out.println(test.toString());
-    System.out.println("test length "+test.length());
-    System.out.println(test5);
-    System.out.println("test2 length "+test2.length());
-    MyString lowercase = test.toLowerCase();
-    System.out.println(lowercase.toString());
-    MyString uppercase = test2.toUpperCase();
-    System.out.println(uppercase.toString());
-    System.out.println(test.toString());
-    System.out.println(test4);
-    System.out.println(test3.valueOf(2));
-    if (test.compareTo(test3)==0)
-      System.out.println("yes");
+    char[] Hello = {'H','e','l','l','o'};
+    char[] hello = {'h','e','l','l','o'};
+    char[] alphaCase = {'U','P','P','E','R','l','o','w','e','r'};
+    MyString HelloString = new MyString(Hello);
+    MyString helloString = new MyString(hello);
+    MyString caseString = new MyString(alphaCase);
+    System.out.println("HelloString consists of: "+HelloString.getMyString());
+    System.out.println("Character at 4th index is: "+HelloString.charAt(4));
+    System.out.println("Length of HelloString is: "+HelloString.length());
+    System.out.println("Substring of HelloString at 0th and 3rd indices is: "+HelloString.substring(0,4));
+    System.out.println("This is all lowercase: "+caseString.toLowerCase());
+    System.out.println("This is all uppercase: "+caseString.toUpperCase());
+    System.out.println("Comparing "+HelloString+" and "+helloString+" results in "+HelloString.compareTo(helloString));
+    String test = HelloString.toString();
+    System.out.println("This is a string: "+test);
+    char[] empty = new char[1];
+    MyString emptyString = new MyString(empty);
+    System.out.println("The value of int 234 in MyString is "+emptyString.valueOf(234));
   }
 }
--- HW3/MyString.java
+++ HW3/MyString.java
@@ -1,64 +1,83 @@
-final class MyString {
-  private char[] charArray;
+import java.util.*;
 
-  public MyString(char[] chars) {
-    this.charArray = chars;
+final class MyString {
+  private char[] charArray; //Charact array as private variable
+
+  public MyString(char[] chars) { //Constructor
+    this.charArray = chars; //Copy chars argument into this instance
   }
 
   public char charAt(int index) {
-    char[] temp = charArray;
-    return temp[index];
+    char[] temp = charArray; 
+    if ((index>=temp.length)||(index<0)) //Check if index is out of array bounds
+      return 0;
+    else return temp[index]; //Returns char at index if within bounds
   }
 
   public int length() {
-    return this.charArray.length;
+    return this.charArray.length; //Return length of this instance's array
   }
 
   public MyString substring(int begin, int end) {
-    char[] temp = new char[end-begin];
-    for (int i=begin; i<end; i++) {
-      temp[i-begin] = this.charArray[begin];
-    }
-    return new MyString(temp);
+    if (end<begin)
+      return this;
+    char[] temp = new char[end-begin]; //Create temp array with length of end-begin args
+    for (int i=begin; i<end; i++)
+      temp[i-begin] = this.charArray[i]; //Copy elements starting from 'begin' to 'end'
+    return new MyString(temp); //Returns the copied elements as new MyString
   }
 
   public MyString toLowerCase() {
-    char[] temp = new char[this.charArray.length];
+    char[] temp = new char[this.charArray.length]; //New temp array of same length
     for (int i=0; i<this.charArray.length; i++)
-      temp[i] = Character.toLowerCase(this.charArray[i]);
-    return new MyString(temp);
+      temp[i] = Character.toLowerCase(this.charArray[i]); //Change each element to lowercase
+    return new MyString(temp); //Return temp array as new MyString
   }
 
   public MyString toUpperCase() {
     char[] temp = new char[this.charArray.length];
     for (int i=0; i<this.charArray.length; i++)
-      temp[i] = Character.toUpperCase(this.charArray[i]);
-    return new MyString(temp);
+      temp[i] = Character.toUpperCase(this.charArray[i]); //Change each element to uppercasse
+    return new MyString(temp); //Return tempa rrayas new MyString
   }
 
   public int compareTo(MyString s) {
-    if (s.length()==this.charArray.length) {
-      for (int i=0; i<s.length(); i++) {
-        if (s.charAt(i)!=this.charArray[i])
-          return 1;
-      }
-      return 0;
-    } else return 1;
+    char[] temp = this.charArray;
+    int length = Math.min(s.length(), temp.length); //Takes the lesser of two lengths
+    if (s.length()!=temp.length)
+      return (s.length()-temp.length); //If lengths are different, return difference between them
+    for (int i=0; i<length; i++) { //Loop through the shorter length to avoid out-of-bounds
+      if (s.charAt(i)!=temp[i])
+        return (s.charAt(i)-temp[i]); //If characters are different, return difference between them
+    }
+    return 0; //Return 0 if everything ends up being the same
   }
 
   public MyString getMyString() {
-    char[] temp = this.charArray;
+    char[] temp = this.charArray; //Copies current instance and returns it as new MyString
     return new MyString(temp);
   }
 
   public String toString() {
-    String temp = new String(this.charArray);
+    String temp = new String(this.charArray); //Copies contents of this instance's array into a new String
     return temp;
   }
 
+// Consulted https://www.geeksforgeeks.org/collections-reverse-java-examples/ on how to
+// reverse a list and the Character class api to convert ints to chars as well as
+// https://www.javatpoint.com/java-int-to-char
   public static MyString valueOf(int i) {
-    char[] temp = new char[1];
-    temp[0] = (char) i;
-    return new MyString(temp);
+    List<Character> toChars = new ArrayList<Character>();
+    int dig = 0;
+    while (i>0) {
+      dig = i%10; //Takes remainder of the arg
+      i = i/10; //Removes right most digit
+      toChars.add(Character.forDigit(dig, 10)); //Adds remainder to list
+    }
+    Collections.reverse(toChars); //Reverse the list after all digits taken
+    char[] temp = new char[toChars.size()]; //Declare char array of same size
+    for (int j=0; j<toChars.size(); j++)
+      temp[j] = toChars.get(j); //Copy elements from list to char array
+    return new MyString(temp); //Return char array in new MyString
   }
 }
