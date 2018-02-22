e3a2a0d73143501c567add06836403a4ab84391d
CChiu28 <ScorpiusDX@gmail.com>
2018-02-22 11:36:41 -0500

    hw4

--- HW4/Main.java
+++ HW4/Main.java
@@ -1,10 +1,16 @@
 public class Main {
-    public void main(String[] args) {
-        char[][] test = new char[1][1];
-        for (int i=0; i<test.length; i++) {
-            for (int j=0; j<test[i].length; j++) {
-                
-            }
-        }
+  public static void main(String[] args) {
+    char[][] test = { 
+      {'T','t'},
+      {'E','e'},
+      {'S','s'},
+      {'T','t'}};
+    for (int i=0; i<test.length; i++) {
+      for (int j=0; j<test[0].length; j++) {
+        System.out.print(test[i][0]+" "+test[i][j]+"\n");
+      }
     }
+    System.out.println("length of row: "+test.length);
+    System.out.println("length of col: "+test[0].length);
+  }
 }
