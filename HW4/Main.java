9397628588461f21bc90747eeca5c1a4e85c25a3
CChiu28 <ScorpiusDX@gmail.com>
2018-02-22 00:37:31 -0500

    initial hw4

---
+++ HW4/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public void main(String[] args) {
+        char[][] test = new char[1][1];
+        for (int i=0; i<test.length; i++) {
+            for (int j=0; j<test[i].length; j++) {
+                
+            }
+        }
+    }
+}
