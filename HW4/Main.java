6162fe3c54279de3f64a66aae9046b32da9387c3
CChiu28 <ScorpiusDX@gmail.com>
2018-02-26 15:15:08 -0500

    hw4

--- HW4/Main.java
+++ HW4/Main.java
@@ -1,39 +1,31 @@
+import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
     char[][] test = { 
-      {'a','b'},
+      {'a','b','h','i'},
       {'c','d'},
       {'e','f','g'}};
     int[] counter = new int[test.length];
-    int combo = 0;
+    int combo=0;
     int totalCombos = 1;
-    int tick = 0;
+    int tick = 1;
     for (int i=0; i<test.length; i++)
       totalCombos *= test[i].length;
+    char[][] result = new char[totalCombos][test.length];
     while (combo<totalCombos) {
       for (int i=0; i<test.length; i++) {
-        System.out.print(test[i][counter[i]]+" ");
-        if (i==test.length-1) {
-          if (counter[i]==test[i].length-1&&i>=1) {
-            counter[i] = 0;
-            counter[i-1] += 1;
-          } else counter[i] += 1;
-        }
+        result[combo][i] = test[i][counter[i]];
       }
-      System.out.println();
+      for (int j=test[0].length-1; j>=0; j--) {
+        counter[j] += 1;
+      }
       combo++;
     }
-    // // int check1 = 0;
-    // // int check2 = 0;
-    // int check3 = 0;
-    // // int numCombo = 1;
-    // // for (int i=0; i<test.length; i++)
-    // //   numCombo *= test[i].length;
-    // for (int i=0; i<test[i].length; i++) {
-    //   for (int j=0; j<test.length; j++)
-    //   System.out.println();
-    // }
-    // System.out.println("length of row: "+test.length);
-    // System.out.println("length of col: "+test[0].length);
+    for (int i=0; i<result.length; i++) {
+      for (int j=0; j<result[i].length; j++) {
+        System.out.print(result[i][j]+" ");
+      }
+      System.out.println();
+    }
   }
 }
