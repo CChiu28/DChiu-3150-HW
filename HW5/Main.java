17fd47dce78bc972bfe9ec9dc393eb5fbe034b30
CChiu28 <ScorpiusDX@gmail.com>
2018-03-06 11:54:34 -0500

    hw5

--- HW5/Main.java
+++ HW5/Main.java
@@ -1,11 +1,20 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Random rand = new Random();
         int r = 5;
+        int x = 0;
+        int y = 0;
         int sqArea = r*r;
         double cirArea = (Math.PI*Math.pow(r, 2))/4;
         double areaRatio = cirArea/sqArea;
         System.out.println("Sq: "+sqArea);
         System.out.println("Circle: "+cirArea);
         System.out.println("Ratio: "+areaRatio);
+        for (long i=0; i<10; i++) {
+            x = rand.nextInt(6);
+            y = rand.nextInt(6);
+        }
     }
 }
