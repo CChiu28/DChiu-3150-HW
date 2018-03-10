b07cb96a305fb562c6cda3e72938df648c73aa21
CChiu28 <ScorpiusDX@gmail.com>
2018-03-10 18:00:45 -0500

    hw6

---
+++ HW6/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 0; //number of queens
+        int[] grid = new int[n]; //Size of board 'n'
+        int[] queenPos = new int[n]; //Position of queens
+    }
+}
