cd85ada1621e0e1d26d8258917d63e4f5712690d
CChiu28 <ScorpiusDX@gmail.com>
2018-03-10 18:46:27 -0500

    hw6

--- HW6/Main.java
+++ HW6/Main.java
@@ -3,5 +3,11 @@ public class Main {
         int n = 0; //number of queens
         int[][] grid = new int[n][n]; //Size of board 'n'
         int[][] queenPos = new int[n][n]; //Position of queens
+        boolean isSafe = true;
+        for (int row=0; row<n; row++) {
+            for (int col=0; col<n; col++) {
+                
+            }
+        }
     }
 }
