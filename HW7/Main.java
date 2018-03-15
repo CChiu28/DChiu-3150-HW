96656085d5ac3a0b1158e9ec202fa65693f350b3
CChiu28 <ScorpiusDX@gmail.com>
2018-03-15 14:21:40 -0400

    hw7

--- HW7/Main.java
+++ HW7/Main.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "";
+        StringBuffer input = new StringBuffer();
+        for (int i=0; i<args.length; i++)
+            input.append(args[i]);
+        System.out.print(input);
     }
 }
