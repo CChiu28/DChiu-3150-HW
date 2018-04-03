36900a4394c56d817f94fd02181d6df4e4d9d885
CChiu28 <ScorpiusDX@gmail.com>
2018-04-03 18:21:27 -0400

    hw7

--- HW7/Main.java
+++ HW7/Main.java
@@ -1,16 +1,40 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        StringBuffer input = new StringBuffer();
-        for (int i=0; i<args.length; i++)
-            input.append(args[i]);
-        System.out.print(input);
+        String[] input = new String[args.length];
+        // char[] cArray = args[].toCharArray();
+        try {
+            if (args.length==0)
+                System.out.println("Empty");
+            else for (int i=0; i<args.length; i++) {
+                if (Character.isAlphabetic(args[i].charAt(0))) {
+                    throw new LookAtMrAlgebraOverHereException();
+                }
+                input[i] = args[i];
+            }
+        } catch(LookAtMrAlgebraOverHereException e) {
+            System.out.println("Enter a real number kthx");
+            System.exit(1);
+        }
+        System.out.println(Arrays.toString(input));
+        try {
+            calc(input);
+        } catch(RuntimeException e) {}
+        System.exit(1);
+    }
+
+    public static void calc(String[] s) throws RuntimeException {
+
     }
 }
 
 class LookAtMrAlgebraOverHereException extends IllegalArgumentException {
     public LookAtMrAlgebraOverHereException() {}
+    public LookAtMrAlgebraOverHereException(String s) {
+        System.out.println(s);
+    }
 }
 
 class IllegalOperationException extends IllegalArgumentException {
