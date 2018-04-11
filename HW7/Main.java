221de38c64433223bf71dd81c81aacd5e77ac573
CChiu28 <ScorpiusDX@gmail.com>
2018-04-11 02:46:13 -0400

    hw7

--- HW7/Main.java
+++ HW7/Main.java
@@ -14,8 +14,10 @@ public class Main {
                     throw new LookAtMrAlgebraOverHereException("Enter a real number kthx");
                 } else if (i>0&&checkDigit(args[i])&&checkDigit(args[i-1])) {
                     throw new LookAtMrAlgebraOverHereException("bruh you entered 2 numbers in a row");
-                } else if (i>0&&checkOperator(args[i])&&checkOperator(args[i-1]))
+                } else if (i>0&&checkOperator(args[i])&&checkOperator(args[i-1])) {
                     throw new LookAtMrAlgebraOverHereException("bruh you entered 2 operators in a row");
+                } else if (i==0&&checkOperator(args[i]))
+                    throw new UserIsADumbassException("Bruh you forgot a number");
                 input[i] = args[i];
             }
         } catch(LookAtMrAlgebraOverHereException e) {
@@ -23,30 +25,47 @@ public class Main {
         }
         System.out.println(Arrays.toString(input));
         try {
-            postfix(input);
-        } catch(RuntimeException e) {}
+            String[] postfix = new String[postfix(input).length()];
+            postfix = postfix(input).split(" ");
+            System.out.println(Arrays.toString(postfix)+" "+postfix.length);
+            System.out.println(calc(postfix));
+        } catch(RuntimeException e) {
+            System.out.println("bruh..."+e);
+            e.printStackTrace();
+        }
         System.exit(1);
     }
 
-    public static double calc(String a, String op, String b) throws RuntimeException {
-        double x = Double.parseDouble(a);
-        double y = Double.parseDouble(b);
-        if (op.equals("*")) {
-            return x*y;
-        } else if (op.equals("/")) {
-            if (y==0)
-                throw new ArithmeticException("You destroyed the universe by dividing by 0");
-            else return x/y;
-        } else if (op.equals("+")) {
-            return x+y;
-        } else if (op.equals("-")) {
-            return x-y;
-        } else if (op.equals("%")) {
-            return x%y;
-        } else throw new IllegalOperationException("bro lrn2math");
+    public static double calc(String[] exp) {
+        double x = 0;
+        double y = 0;
+        double result = 0;
+        Stack<String> calc = new Stack<>();
+        for (String s:exp) {
+            if (checkDigit(s))
+                calc.push(s);
+            else {
+                y = Double.parseDouble(calc.pop());
+                x = Double.parseDouble(calc.pop());
+                if (s.equals("*")) {
+                    calc.push(Double.toString(x*y));
+                } else if (s.equals("/")) {
+                    if (y==0)
+                        throw new ArithmeticException("You destroyed the universe by dividing by 0");
+                    else calc.push(Double.toString(x/y));
+                } else if (s.equals("+")) {
+                    calc.push(Double.toString(x+y));
+                } else if (s.equals("-")) {
+                    calc.push(Double.toString(x-y));
+                } else if (s.equals("%")) {
+                    calc.push(Double.toString(x%y));
+                } else throw new IllegalOperationException("bro lrn2math");
+            }
+        }
+        return Double.parseDouble(calc.pop());
     }
 
-    public static double postfix(String[] s) throws RuntimeException {
+    public static String postfix(String[] s) throws RuntimeException {
         Stack<Character> operators = new Stack<>();
         StringBuffer exp = new StringBuffer();
         char paren;
@@ -75,7 +94,7 @@ public class Main {
             exp.append(operators.pop()+" ");
         System.out.println("digs "+exp.toString());
         // System.out.println(result);
-        return result;
+        return exp.toString();
     }
 
     public static boolean checkDigit(String s) {
