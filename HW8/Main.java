6d58ae2dc73812d60de48b6b0acdb113d5673745
CChiu28 <ScorpiusDX@gmail.com>
2018-04-29 00:24:09 -0400

    hw8

--- HW8/Main.java
+++ HW8/Main.java
@@ -1,15 +1,39 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        File root = new File(args[0]);
-        if (!root.exists())
+        File root = new File(args[0]); // Create new File obj with inputted filepath
+        StringBuffer tree = new StringBuffer(); // Stringbuffer for creating tree
+        if (!root.exists()) { // If invalid filepath
             throw new IllegalArgumentException("No dir here");
-        else {
-            System.out.println(Arrays.toString(root.list()));
-            StringBuffer tree = new StringBuffer();
-            
+        } else {
+            tree.append(root.getPath() + ":\n"); // Start with inputted filepath
+            getDir(root, tree, 0); // Call method to construct tree
+            try {
+                BufferedWriter write = new BufferedWriter(new FileWriter("dir_tree.txt")); // Create new txt file for output
+                write.append(tree); // Write stringbuffer to txt file
+                write.close(); // Close
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // System.out.println(tree);
+    }
+
+    // This method takes 3 variables: the File obj, the StringBuffer obj, and an int that designates 
+    // the level of the directory
+    public static void getDir(File root, StringBuffer write, int level) {
+        for (File file : root.listFiles()) { // Get list of dir/file in current File obj and assign temp File to each per loop
+            for (int i=0; i<level; i++) // For each dir sublevel, output an indentation
+                write.append("   ");
+            if (file.isDirectory()) { // If current File is a directory path...
+                write.append("|--"); // output branch
+                write.append(file.getName()+"/\n"); // Get directory name
+                getDir(file, write, level+1); // And call getDir method recursively with sublevel+1 to get dir/files in this dir
+            } else if (file.isFile()) { // If File is a file...
+                write.append("|--"); // output branch
+                write.append(file.getName()+"\n"); // and filename
+            }
         }
     }
 }
